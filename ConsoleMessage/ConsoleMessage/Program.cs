c3ea39dd69cff7da2ced563c11e2919ae7f85c0d
Ionut-Antoniu <ioni_gherlan@yahoo.com>
2023-02-22 15:45:57 +0200

    ConsoleApp

---
+++ ConsoleMessage/ConsoleMessage/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMessage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Greetings,");
+            Console.WriteLine("Have fun today learning new things !");
+            Console.WriteLine("Tell me your name:");
+
+            string userName = Console.ReadLine();
+            Console.WriteLine("So, your name is: " + userName);
+            
+        }
+    }
+}
